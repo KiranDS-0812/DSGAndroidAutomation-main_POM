a5d9c35ad49ef901975c23f772b739a0dbf5ffd6
kirands0812 <kirands0812@gmail.com>
2026-04-27 07:07:04 +0000

    Add automation scripts

---
+++ app/src/main/kotlin/dsg/tests/identifiers/TestTags.kt
@@ -0,0 +1,13 @@
+package dsg.tests.identifiers
+
+/**
+ * Test Tags for Onboarding UI Elements
+ * Contains constants for identifying UI elements in Espresso tests
+ */
+object TestTags {
+    const val LOGO = "onboarding_logo"
+    const val JOIN_NOW = "join_now_button"
+    const val GUEST = "guest_button"
+    const val CONTINUE = "continue_button"
+    const val NOT_NOW = "not_now_button"
+}
